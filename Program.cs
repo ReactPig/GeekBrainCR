119426f9f1bf640bc87d4611c1188e7b237e7053
ReactPig <125767193+ReactPig@users.noreply.github.com>
2023-02-25 00:17:32 +0300

    Добавим решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] values1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] values2 = new string[values1.Length];
+void SecondArrayWithIF(string[] values1, string[] values2)
+{
+    int count = 0;
+    for (int i = 0; i < values1.Length; i++)
+    {
+    if(values1[i].Length <= 3)
+        {
+        values2[count] = values1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(values1, values2);
+PrintArray(values2);
